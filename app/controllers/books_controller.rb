72bb7dcd55f034e3ed14c7838a13b365f2ce28a1
myhearisranch <myhearisranch@gmail.com>
2021-08-14 09:29:52 +0000

    index create newアクション　追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,31 @@
 class BooksController < ApplicationController
+
+  def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    #空のモデルが生成され、インスタンス変数@listに代入されてViewで利用できるようになります。
+    @book = Book.new
+  end
+
+  def create
+     # １. データを新規登録するためのインスタンス作成
+     #list_paramsではフォームで入力されたデータを受け取っています。
+    book = Book.new(book_params)
+     # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+     # ３. トップ画面へリダイレクト
+     redirect_to '/'
+
+
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
